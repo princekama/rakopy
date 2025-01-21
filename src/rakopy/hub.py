53032709d32913c8a9484e17adc0da73bd7a0c3d
Prince Kamalanathan <33226588+princekama@users.noreply.github.com>
2025-01-21 23:42:18 +0000

    Fix pyling errors

--- src/rakopy/hub.py
+++ src/rakopy/hub.py
@@ -407,24 +407,25 @@ class Hub:
         Converts JSON data to Room.
         """
         channels = []
-        channel1P = True
-        colorType1 = None
-        colorTitle1 = None
-        multiChannelComponent1 = None
+        channel_1 = True
+        color_type_1 = None
+        color_title_1 = None
+        multi_channel_component_1 = None
         for channel in data["channel"]:
-            if channel1P:
-                channel1P = False
-                colorType1 = channel.get("colorType", None)
-                colorTitle1 = channel.get("colorTitle", None)
-                multiChannelComponent1 = channel.get("multiChannelComponent", None)
+            if channel_1:
+                channel_1 = False
+                color_type_1 = channel.get("colorType", None)
+                color_title_1 = channel.get("colorTitle", None)
+                multi_channel_component_1 = channel.get("multiChannelComponent", None)
+
             channels.append(
                 Channel(
                     id = channel["channelId"],
                     title = channel["title"],
                     type = channel["type"],
-                    color_type = channel.get("colorType", colorType1),
-                    color_title = channel.get("colorTitle", colorTitle1),
-                    multi_channel_component = channel.get("multiChannelComponent", multiChannelComponent1)
+                    color_type = channel.get("colorType", color_type_1),
+                    color_title = channel.get("colorTitle", color_title_1),
+                    multi_channel_component = channel.get("multiChannelComponent", multi_channel_component_1)
                 )
             )
 
